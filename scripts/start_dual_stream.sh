23001ba8535e6dcae28307be51df53e164f38f2e
Jannes Höke <mail@jhoeke.de>
2020-03-27 16:58:11 +0100

    🍱 Update scripts

--- scripts/start_dual_stream.sh
+++ scripts/start_dual_stream.sh
@@ -7,12 +7,13 @@ ffmpeg \
 -f lavfi -i anullsrc \
 -c:v libx264 \
 -b:v 3000k \
--acodec libmp3lame -ar 44100 -b:a 128k \
--preset medium \
 -pix_fmt yuv420p \
+-preset medium \
 -r 30 \
--g 10 \
+-g 60 \
 -s 1280x720 \
+-acodec libmp3lame -ar 44100 -b:a 128k \
 -threads 0 \
+-async 1 -vsync 1 \
 -f tee -map 0:v -map 1:a \
 "[f=flv]rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY_1}|[f=flv]rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY_2}"
---
+++ scripts/start_single_stream.sh
@@ -0,0 +1,18 @@
+ffmpeg \
+-f x11grab \
+-s 1600x900 \
+-framerate 30 \
+-draw_mouse 0 \
+-i :20 \
+-f lavfi -i anullsrc \
+-c:v libx264 \
+-b:v 3000k \
+-pix_fmt yuv420p \
+-preset medium \
+-r 30 \
+-g 60 \
+-s 1280x720 \
+-acodec libmp3lame -ar 44100 -b:a 128k \
+-threads 0 \
+-async 1 -vsync 1 \
+-f flv "rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}"
